b022cec3ae379e1df81f00bc0060df6d8f53a8ea
c20245792.arevalo <c20245792.arevalo@csav.edu.ph>
2026-09-04 15:40:28 +0800

    functions

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ console.log(`Age:${age}`);
 console.log(`Number: ${myNumber} `);
 console.log(`Address: ${myAddress}`);
 console.log(`${67}`);
+
+function greet(greetings, names = []) {
+    return `${greetings} sa emu, ${names}`;
+}
+console.log(greet("Maayong aga", ["Wency", "Bai", "Soy", "Tol", "Erp"]));
+
+
+
