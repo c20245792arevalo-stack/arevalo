adde87f2585e81b43bcca55c31bfa67273223177
c20245792.arevalo <c20245792.arevalo@csav.edu.ph>
2026-09-04 14:39:39 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log("Hello World.");
 console.log("From script.js file.");
+
+const myName = "Wency";
+let age = 21;
+let myNumber = 997717381;
+let myAddress = "Ditching Subd, Brgy V.";
+
+
+console.log(`Name: ${myName}`);
+console.log(`Age:${age}`);
+console.log(`Number: ${myNumber} `);
+console.log(`Address: ${myAddress}`);
+console.log(`${67}`);
